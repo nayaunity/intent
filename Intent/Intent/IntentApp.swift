f178c952e4bf468de35d1ee0b0000b59ef1b20bd
NyaradzoB <79171484+NyaradzoB@users.noreply.github.com>
2023-09-03 19:30:39 -0400

    create new project

---
+++ Intent/Intent/IntentApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntentApp.swift
+//  Intent
+//
+//  Created by Nyaradzo Bere on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct IntentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
