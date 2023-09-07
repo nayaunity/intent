837fd522f76a4d50243961b9de9bc69f2726b330
NyaradzoB <79171484+NyaradzoB@users.noreply.github.com>
2023-09-07 15:39:16 -0400

    add average ratings and categories to match profiles

--- Intent/Intent/Views/Profile/ProfileView.swift
+++ Intent/Intent/Views/Profile/ProfileView.swift
@@ -7,18 +7,17 @@
 
 import Foundation
 import SwiftUI
-import FirebaseAuth
-import FirebaseFirestore
 import SDWebImageSwiftUI
+import Firebase
 
 struct ProfileView: View {
     var user: User
-
+    @State private var averageRatings: [String: Double] = [:]
     @State private var isRatingViewPresented = false
 
     var body: some View {
         NavigationView {
-            ScrollView {
+            ScrollView(showsIndicators: false) {
                 VStack {
                     WebImage(url: URL(string: user.profilePictureUrl))
                         .resizable()
@@ -36,7 +35,18 @@ struct ProfileView: View {
                         .foregroundColor(.gray)
                         .padding()
 
-                    // ... Add more user details here if needed
+                    // Display average ratings or "No ratings yet" text
+                    if averageRatings.isEmpty {
+                        Text("No ratings yet")
+                            .font(.headline)
+                            .padding()
+                    } else {
+                        ForEach(averageRatings.sorted(by: { $0.key < $1.key }), id: \.key) { (category, rating) in
+                            Text("\(category.capitalized) Rating: \(String(format: "%.1f", rating))")
+                                .font(.headline)
+                                .padding(.bottom)
+                        }
+                    }
 
                     // "We went on a date" button
                     NavigationLink(
@@ -57,9 +67,57 @@ struct ProfileView: View {
                     .padding()
                 }
             }
-//            .navigationBarTitle("Profile", displayMode: .inline)
             .navigationBarHidden(true)
+            .onAppear {
+                // Calculate and load the average ratings
+                calculateAverageRatings()
+            }
+            .onAppear {
+                UIScrollView.appearance().showsVerticalScrollIndicator = false
+            }
         }
+    }
+
+    private func calculateAverageRatings() {
+        let db = Firestore.firestore()
+        let ratingsCollection = db.collection("ratings")
+        
+        ratingsCollection.whereField("ratedUserID", isEqualTo: user.id ?? "")
+            .getDocuments { (snapshot, error) in
+                if let error = error {
+                    print("Error fetching ratings: \(error)")
+                    return
+                }
+
+                var totalRatings: [String: Double] = [:]
+                var ratingCounts: [String: Int] = [:]
+
+                for document in snapshot?.documents ?? [] {
+                    let data = document.data()
+                    let promptnessRating = data["promptnessRating"] as? Int ?? 0
+                    let respectfulnessRating = data["respectfulnessRating"] as? Int ?? 0
+                    let comfortabilityRating = data["comfortabilityRating"] as? Int ?? 0
+                    let presentationRating = data["presentationRating"] as? Int ?? 0
+                    let conversationQualityRating = data["conversationQualityRating"] as? Int ?? 0
+
+                    totalRatings["promptness"] = (totalRatings["promptness"] ?? 0) + Double(promptnessRating)
+                    totalRatings["respectfulness"] = (totalRatings["respectfulness"] ?? 0) + Double(respectfulnessRating)
+                    totalRatings["comfortability"] = (totalRatings["comfortability"] ?? 0) + Double(comfortabilityRating)
+                    totalRatings["presentation"] = (totalRatings["presentation"] ?? 0) + Double(presentationRating)
+                    totalRatings["conversationQuality"] = (totalRatings["conversationQuality"] ?? 0) + Double(conversationQualityRating)
+
+                    ratingCounts["promptness"] = (ratingCounts["promptness"] ?? 0) + 1
+                    ratingCounts["respectfulness"] = (ratingCounts["respectfulness"] ?? 0) + 1
+                    ratingCounts["comfortability"] = (ratingCounts["comfortability"] ?? 0) + 1
+                    ratingCounts["presentation"] = (ratingCounts["presentation"] ?? 0) + 1
+                    ratingCounts["conversationQuality"] = (ratingCounts["conversationQuality"] ?? 0) + 1
+                }
+
+                for (key, value) in totalRatings {
+                    let average = value / Double(ratingCounts[key] ?? 1)
+                    self.averageRatings[key] = average
+                }
+            }
     }
 }
 
